cf4015cec88c7bb9148f658c1be6c86ffe37300f
Arjun Ramaswami <arjunr@mail.upb.de>
2019-12-16 13:57:52 +0100

    bash script: using OMP affinity instead of srun bind

--- fftw/omp_fftw_run.sh
+++ fftw/omp_fftw_run.sh
@@ -3,6 +3,7 @@
 #SBATCH -J fftw_ref
 #SBATCH -p batch
 #SBATCH -N 1
+#SBATCH -t 1:29:00
 
 ## Execute fftw multithreaded code 
 ##   Arg : Sizes of FFT to execute
@@ -15,16 +16,26 @@ module load toolchain/gompi
 module load numlib/FFTW
 
 current_time=$(date "+%Y.%m.%d-%H.%M")
-iteration=1
+iteration=10
 
+## Set OMP Environment Variables
+export OMP_DISPLAY_AFFINITY=TRUE
+export OMP_DISPLAY_ENV=TRUE
+
+export OMP_PLACES=cores
+export OMP_PROC_BIND=close
+
+# Execute applications from 1 to 40 threads one after another
 echo "Passed $# FFT3d Sizes"
 for arg in $@
 do
     echo "Executing FFT Size : $arg $arg $arg"
     for thread in {1..40}
     do
+        echo "Running with number of threads : $thread"
+        export OMP_NUM_THREADS=${thread}
         #echo " Num of threads : $thread " >> omp_${arg}_fft_${current_time}
-        ./host_omp -m $arg -n $arg -p $arg -i ${iteration} -t ${thread} >> data/omp_${arg}_fft_${current_time}
+        srun ./host_omp -m $arg -n $arg -p $arg -i ${iteration} -t ${thread} >> data/omp_${arg}_fft_${current_time}
     done
 done
 
