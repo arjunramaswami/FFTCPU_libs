2c4f4f089d3a2db57c4ced8fce0d3d61a9bd0efd
Arjun Ramaswami <arjunr@mail.upb.de>
2019-12-20 13:07:55 +0100

    working fft3d
    TODO: input cmd line args
    TODO: better FFT3d input and verification data

--- mkl/main.c
+++ mkl/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <float.h>
 
 /*
 *  Include FFT Files
@@ -21,9 +23,8 @@
 *         : size of FFT3d - N1, N2, N3
 *  Output : 1 if error in initialization
 */
-static int init(MKL_Complex16 *fft_data, int N1, int N2, int N3){
-    int where = 0;
-    int i, j, k;
+static void init(MKL_Complex16 *fft_data, int N1, int N2, int N3){
+    int where = 0, i, j, k;
 
     for(i = 0; i < N1; i++){
         for(j = 0; j < N2; j++){
@@ -34,43 +35,56 @@ static int init(MKL_Complex16 *fft_data, int N1, int N2, int N3){
             }
         }
     }
-
 }
 
-/* 
-*  Initialize input array to FFT
-*  Input  : pointer to initialized 3d array of MKL_Complex16 type 
-*         : size of FFT3d - N1, N2, N3
-*         : num_threads 
-*  Output : 1 if error in execution
-*/
-static int run_fft(MKL_Complex16 *fft_data, int N1, int N2, int N3, int num_threads){
-    int i, j, k, where;
-    MKL_LONG status = 0;
-    printf("Create DFTI descriptor for %ix%ix%i FFT\n", N1, N2, N3);
-
-    DFTI_DESCRIPTOR_HANDLE ffti_desc_handle;
-
-    MKL_LONG dim = 3;
-    MKL_LONG size[3];
-    size[0] = N1;
-    size[1] = N2;
-    size[2] = N3;
-
-    /* Configuration
-    * ------------------
-    * ffti_desc_handle - fft intel description handle / DFTI_DESCRIPTOR HANDLE type
-    * double precision - precision                    / enum MACRO 
-    * DFTI_COMPLEX     - data type or domain          / enum MACRO
-    *            3     - dimension                    / MKL_LONG
-    *   {64,64,64}     - points in each dimension     / array of MKL_LONG
-    * ------------------- */
-    status = DftiCreateDescriptor( &ffti_desc_handle, DFTI_DOUBLE, DFTI_COMPLEX, dim, size);
+static void error_msg(MKL_LONG status){
     if(status != DFTI_NO_ERROR){
         char *error_message = DftiErrorMessage(status);
-        printf("MKL FFT Description Creation Failed with message %s \n", error_message);
-        return 1;
+        printf("Failed with message %s \n", error_message);
+        exit(1);
     }
+}
+
+int main(){
+    int err = 0, where = 0, iter = 1;
+    int i, j, k;
+    int N1 = 8, N2 = 8, N3 = 8;
+    MKL_LONG status = 0;
+
+    // Print Version of Intel MKL
+    char version[DFTI_VERSION_LENGTH];
+    DftiGetValue(0, DFTI_VERSION, version);
+
+    // Multi threaded
+    int thread_id = 0, team = 1;
+#if defined(_OPENMP)
+    thread_id = omp_get_thread_num();
+    team  = omp_get_num_threads();
+    printf("Total number of threads %d \n", team);
+#endif
+
+    // Command Line Arguments
+    MKL_LONG dim = 3;
+    MKL_LONG size[3]; size[0] = N1; size[1] = N2; size[2] = N3;
+
+    printf("\n------------------------------------\n");
+    printf("MKL Configuration: \n");
+    printf("------------------------------------\n");
+    printf("MKL VERSION : %s\n", version);
+    printf("Forward and Backward Double precision complex 3d FFT");
+    printf("Parameters: \n");
+    printf("DFTI_DIMENSION      =  3\n");
+    printf("DFTI_PRECISION      = DFTI_DOUBLE \n");
+    printf("DFTI_LENGTHS        = {%i, %i, %i} \n", size[0], size[1], size[2]);
+    printf("DFTI_FORWARD_DOMAIN = DFTI_COMPLEX\n");
+    printf("DFTI_PLACEMENT      = DFTI_INPLACE\n");
+    printf("THREADS             = %i \n", team);
+    printf("------------------------------------\n\n");
+
+    printf("Create DFTI descriptor\n");
+    DFTI_DESCRIPTOR_HANDLE ffti_desc_handle = 0;
+    status = DftiCreateDescriptor( &ffti_desc_handle, DFTI_DOUBLE, DFTI_COMPLEX, dim, size);
+    error_msg(status);
 
     //status = DftiSetValue(ffti_desc_handle, DFTI_CONJUGATE_EVEN_STORAGE, DFTI_COMPLEX_COMPLEX);
     //status = DftiSetValue(ffti_desc_handle, DFTI_OUTPUT_STRIDES, strides_out);
@@ -81,13 +95,9 @@ static int run_fft(MKL_Complex16 *fft_data, int N1, int N2, int N3, int num_thre
         return 1;
     }
 
-    printf("Setting thread limit %i\n", num_threads);
-    status = DftiSetValue(ffti_desc_handle, DFTI_THREAD_LIMIT, num_threads);
-    if(status != DFTI_NO_ERROR){
-        char *error_message = DftiErrorMessage(status);
-        printf("MKL FFT Description Creation Failed with message %s \n", error_message);
-        return 1;
-    }
+    printf("Setting thread limit %i\n", team);
+    status = DftiSetValue(ffti_desc_handle, DFTI_THREAD_LIMIT, team);
+    error_msg(status);
 
     /* Initializes the descriptor
     * Performs optimization
@@ -95,19 +105,27 @@ static int run_fft(MKL_Complex16 *fft_data, int N1, int N2, int N3, int num_thre
     */
     printf("Committing Descriptor\n");
     status = DftiCommitDescriptor(ffti_desc_handle);
-    if(status != 0){
-        char *error_message = DftiErrorMessage(status);
-        printf("MKL FFT Description Creation Failed with message %s \n", error_message);
-        return 1;
+    error_msg(status);
+
+    MKL_Complex16 *fft_data = (MKL_Complex16*)mkl_malloc(N1 * N2 * N3 * sizeof(MKL_Complex16), 64);
+    if (fft_data == NULL){
+        printf("Data allocation failed \n");
+        exit(1);
     }
 
-    printf("Computing Forward Transform\n");
-    status = DftiComputeForward(ffti_desc_handle, fft_data);
-    if(status != 0){
-        char *error_message = DftiErrorMessage(status);
-        printf("MKL FFT Description Creation Failed with message %s \n", error_message);
-        return 1;
+    printf("Initializing Input of %ix%ix%i FFT\n\n", N1, N2, N3);
+    init(fft_data, N1, N2, N3);
+
+    for(i = 0; i < iter; i++){
+        printf("Computing Forward Transform\n");
+        status = DftiComputeForward(ffti_desc_handle, fft_data);
+        error_msg(status);
+
+        printf("Computing Backward Transform\n");
+        status = DftiComputeBackward(ffti_desc_handle, fft_data);
+        error_msg(status);
     }
+
     for(i = 0; i < N1; i++){
         for(j = 0; j < N2; j++){
             for(k = 0; k < N3; k++){
@@ -116,62 +134,10 @@ static int run_fft(MKL_Complex16 *fft_data, int N1, int N2, int N3, int num_thre
             }
         }
     }
-    printf("Computing Backward Transform\n");
-    status = DftiComputeBackward(ffti_desc_handle, fft_data);
-    if(status != 0){
-        char *error_message = DftiErrorMessage(status);
-        printf("MKL FFT Description Creation Failed with message %s \n", error_message);
-        return 1;
-    }
 
     printf("Free descriptor\n");
     status = DftiFreeDescriptor(&ffti_desc_handle);
-    if(status != 0){
-        char *error_message = DftiErrorMessage(status);
-        printf("MKL FFT Description Creation Failed with message %s \n", error_message);
-        return 1;
-    }
-
-    return 0;
-}
-
-
-int main(){
-    int err = 0;
-    int N1 = 64, N2 = 64, N3 = 64;
-    int where = 0;
-    int i, j, k;
-
-    // Print Version of Intel MKL
-    char version[DFTI_VERSION_LENGTH];
-    DftiGetValue(0, DFTI_VERSION, version);
-    printf("%s\n", version);
-
-    // Multi threaded
-    int thread_id = 0, team = 1;
-#if defined(_OPENMP)
-    thread_id = omp_get_thread_num();
-    team  = omp_get_num_threads();
-    printf("Total number of threads %d \n", team);
-#endif
-    
-    MKL_Complex16 *fft_data = (MKL_Complex16*)mkl_malloc(N1 * N2 * N3 * sizeof(MKL_Complex16), 64);
-    if (fft_data == NULL){
-        printf("Data allocation failed \n");
-        return 1;
-    }
-
-    printf("Initializing Input of %ix%ix%i FFT\n", N1, N2, N3);
-    err = init(fft_data, N1, N2, N3);
-    if(err){
-        printf("FFT failed \n");
-    }
-
-    err = run_fft(fft_data, N1, N2, N3, team);
-    if(err){
-        printf("FFT failed \n");
-    }
-
+    error_msg(status);
 
     // free array
     mkl_free(fft_data);
